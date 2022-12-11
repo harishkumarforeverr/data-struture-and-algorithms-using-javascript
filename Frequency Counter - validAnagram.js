0b9fe610e637ff27486d51a512ad2c5e189f174b
harishkumarforever <harishking809@gmail.com>
2022-12-11 16:15:21 +0530

    Create Frequency Counter - validAnagram.js

---
+++ Frequency Counter - validAnagram.js
@@ -0,0 +1,54 @@
+Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+Examples:
+
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram("rat","car") // false) // false
+validAnagram('awesome', 'awesom') // false
+validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+
+
+
+
+///////////////////////////////////////////////////////// Answer /////////////////////////////////////////////////////////////////////////////////////////////////
+function validAnagram(str1,str2) {
+   if(str1.length!==str2.length) return false;
+   const obj1={};
+   const obj2={};
+   str1.split("").forEach((val)=>{
+      obj1[val]=++obj1[val]||1;
+   })
+   str2.split("").forEach((val)=>{
+      obj2[val]=++obj2[val]||1;
+   })
+   for(let val in obj1){
+      if(!(val in obj2)) return false;
+      if(obj2[val]!==obj1[val]) return false
+   }
+   return true;
+}
+
+let res;
+
+res=validAnagram('', '') // true
+console.log("result",res)
+
+res=validAnagram('aaz', 'zza') // false
+console.log("result",res)
+
+res=validAnagram('anagram', 'nagaram') // true
+console.log("result",res)
+
+
+res=validAnagram("rat","car") // false) // false
+console.log("result",res)
+
+res=validAnagram('awesome', 'awesom') // false
+console.log("result",res)
+
+res=validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+console.log("result",res)
+
+
