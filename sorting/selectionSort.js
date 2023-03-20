efe53f3d9f5f659d74e38a91f9037f6084da09d8
harishkumarforever <harishking809@gmail.com>
2023-03-20 19:55:01 +0530

    Create selectionSort.js

---
+++ sorting/selectionSort.js
@@ -0,0 +1,23 @@
+const arr=[9,0,6,3,4,8,8,4,4,6];
+
+const sort=(arr)=>{
+        //  implementing selection sort
+        for(let i=0;i<arr.length;i++){
+                let min=arr[i];
+                let index=i;
+                for(let j=i;j<arr.length;j++){
+                      if(arr[j]<min){
+                        min=arr[j];
+                              index=j
+                      }  
+                }
+                if(index!==i){
+                        let temp=arr[i];
+                        arr[i]=arr[index];
+                        arr[index]=temp;
+                }
+        }
+        return arr;
+}
+
+console.log(sort(arr))
