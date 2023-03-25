b70dc521ed00e9654d9b4c51e2a19ff51324c136
harishkumarforever <harishking809@gmail.com>
2023-03-25 19:15:05 +0530

    Update quickSort.js

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -29,3 +29,52 @@ const quickSort=(arr,left,right)=>{
 } 
 
 console.log(quickSort(arr,0,arr.length-1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///##############################2nd times ps ###################################################################################################################
+ const swap=(arr,i,j)=>{
+    let temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+ }
+// const arr=[1,2,3,4,5,6,7,8,9];
+const arr=[5,9,0,6,3,4,8,8,4,4,6];
+
+const quick=(arr,start,end)=>{
+   let pivotIndex=start+1;
+   let pivotValue=arr[start];
+   for(let i=start+1;i<=end;i++){
+      if(arr[i]<pivotValue){
+         swap(arr,pivotIndex,i);
+         pivotIndex++; 
+      }
+   } 
+   if(pivotIndex!==start+1)
+   swap(arr,pivotIndex-1,start);
+   return pivotIndex-1;
+}
+const quickSort=(arr,start,end)=>{
+     if(start>=end) return arr;
+     const pivot=quick(arr,start,end);
+     quickSort(arr,start,pivot);
+     quickSort(arr,pivot+1,end);
+    return arr;
+   
+  
+}
+
+console.log(quickSort(arr,0,arr.length-1))
